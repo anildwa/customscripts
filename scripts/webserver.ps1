c2ddba878625c9a164e291660ae590962bca18b8
anildwa <anildwa@outlook.com>
2019-03-12 11:00:52 +0530

    Create webserver.ps1

---
+++ scripts/webserver.ps1
@@ -0,0 +1,15 @@
+Configuration webServer
+{
+     Import-DscResource -Module nx
+
+     Node  localhost
+     {
+         nxFile ExampleFile 
+         {
+             DestinationPath = "/tmp/example"
+             Contents = "hello world `n"
+             Ensure = "Present"
+             Type = "File"
+         }
+     }
+}
